901039249ab4748659c6dbc50867e4370797c003
mhsnasln <mhsnasln@gmail.com>
2021-07-19 21:18:52 +0300

    wip

--- main.go
+++ main.go
@@ -8,40 +8,77 @@ import (
 )
 
 type Line struct {
-	Id     string
+	GCode  string
 	Values interface{}
 }
 
 func main() {
 
-	f, _ := os.Open("./import.txt")
+	f, _ := os.Open("./import.gcode")
 
 	scanner := bufio.NewScanner(f)
-	var lines []Line
+	// var lines []Line
 
 	for scanner.Scan() {
 
 		line := scanner.Text()
-		split_line := strings.Split(line, " ")
+		optimze, _ := worker(line)
 
-		number := strings.Split(string(split_line[0]), "N")
+		// Boş geldi dolayısı ile açıklama satırı veya hakkaten boş satır
+		if len(optimze) == 0 {
+			continue
+		}
 
-		lines = append(lines, Line{
-			Id:     string(number[1]),
-			Values: split_line[1:],
-		})
+		fmt.Println(optimze)
+
+		// lines = append(lines, Line{
+		// 	GCode:  string(optimze[1]),
+		// 	Values: optimze[1:],
+		// })
 
 	}
 
-	result, err := worker(lines)
-	if err != nil {
-		panic(err)
-	}
+	// result, err := worker(lines)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Println(result)
+	// fmt.Println(result)
 
 }
 
-func worker(lines []Line) ([]Line, error) {
-	return lines, nil
+func worker(line string) (string, error) {
+
+	// ilk karakter ";"'ise satırı göz ardı et
+	if string(line[0]) == ";" {
+		return "", nil
+	}
+
+	// Dosyadan gelen satırı boşluklarına göre böl
+	split_line_with_space := strings.Split(line, " ")
+
+	// Satırın son kısımda M ile başlayan satır varsa onu sil
+	lastline := split_line_with_space[len(split_line_with_space)-1]
+
+	// Satırın sonında E veya e harfi gördüğünde silecek
+	if string(lastline[0]) == "E" || string(lastline[0]) == "e" {
+		split_line_with_space = append(split_line_with_space[:len(split_line_with_space)-1], split_line_with_space[len(split_line_with_space)-1+1:]...)
+	}
+
+	// ilk satır g0 ise sonuna "M9"
+	if string(split_line_with_space[0]) == "G0" {
+		split_line_with_space = append(split_line_with_space, "M9")
+	}
+
+	// ilk satır g1 ise sonuna "M8"
+	if string(split_line_with_space[0]) == "G1" {
+		split_line_with_space = append(split_line_with_space, "M8")
+	}
+
+	// Parçaladığım veriyi yeniden birleştiriyoruz
+	join_current_line := strings.Join(split_line_with_space, " ")
+
+	// Güncellenen satırı yeniden gönderiyorum
+	return join_current_line, nil
+
 }
