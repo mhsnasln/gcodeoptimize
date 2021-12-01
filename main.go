182e5cc0197213bfe60c61f96c857ff084d726b1
mhsnasln <mhsnasln@gmail.com>
2021-12-01 16:42:34 +0300

    wip

--- main.go
+++ main.go
@@ -10,13 +10,26 @@ import (
 
 func main() {
 
+	// İşlem yapılacak dosyanın adını giriyor
 	var import_name string
+
+	// İşlem yapıldıktan sonra yeni dosyanın ismi
 	var export_name string
 
+	// X değerlerinin artırılma x kadar artırılacak
+	var x_value float64
+
+	// Kullanıcıdan dosya adı istiyoruz!
 	fmt.Print("İşlem yapılacak dosyanın adını girin:")
 	fmt.Scanf("%s", &import_name)
+
+	// Dosya işlem bittikten sonra hangi isimle kaydedilecek
 	fmt.Print("Yeni dosyanın ismini girin:")
 	fmt.Scanf("%s", &export_name)
+
+	// X'ler ne kadar artırılacağını soruyoruz
+	fmt.Print("X Değerini Girin:")
+	fmt.Scanf("%s", &x_value)
 
 	// Dosyamızın yolunu belirtiyoruz
 	file, err := os.ReadFile(import_name)
@@ -24,31 +37,35 @@ func main() {
 		log.Fatalf("İşlem yapılacak dosya bulunamadı: %s", err)
 	}
 
-	// Dosyamızın her bir satırını bize text olarak yansıtacak
+	// Dosyamızın her bir satırını işlemek için alıyoruz
 	scanner := bufio.NewScanner(strings.NewReader(string(file)))
 
+	// Her satır dışarıya dönebilmesi için döngü dışında bir değişken tanımladım. İşlenmiş tüm satırlar burada döngünün tamamlanmasını bekleyecek
 	response := make([]string, 10)
 
+	// Dosya bitene kadar her bir satırda işlem tekrar edeiyor
 	for scanner.Scan() {
 
 		// Dosyadaki her bir satırı alıyoruz
 		line := scanner.Text()
 
-		// Satır işlemlerini ayrıca bir fonksiyonda yapıyorum
+		// Satır işlemleraini ayrıca bir fonksiyonda yapıyorum
 		optimize, err := worker(line)
 		if err != nil {
 			panic("Birşeyler ters gitti.")
 		}
 
-		// Boş geldi dolayısı ile açıklama satırı veya hakkaten boş satır
+		// Kontrol ediyorum satır boş veya açıklama satırı ise atlamasını söylüyorum
 		if len(optimize) == 0 {
 			continue
 		}
 
+		// Her satır işlendikten sonra yeniden kaydedilmesi için bir diziye aktarılıyor
 		response = append(response, optimize)
 
 	}
 
+	// Buraya kadar geldiysek tüm satırlar işlenmiş ve kaydedilmeye hazır
 	wf, err := os.Create(export_name)
 	if err != nil {
 		log.Fatalf("Bir sorun oluştu: %s", err)
@@ -56,6 +73,7 @@ func main() {
 
 	datawriter := bufio.NewWriter(wf)
 
+	// Yine satır satır, açıklama ve boş satırlar olmadan yeni kaydediyorum
 	for _, data := range response {
 		if len(data) == 0 {
 			continue
@@ -66,20 +84,36 @@ func main() {
 	datawriter.Flush()
 	wf.Close()
 
+	fmt.Println("Dosya başarıyla dönüştürüldü!")
+
 }
 
+// bu fonksiyon gelen her satırı işleyip işlenmiş satırı geri gönderiyor
 func worker(line string) (string, error) {
-
-	// ilk karakter ";"'ise satırı göz ardı et
-	if string(line[0]) == ";" {
-		return "", nil
-	}
 
 	// Dosyadan gelen satırı boşluklarına göre böl
 	split_line_with_space := strings.Split(line, " ")
 
+	// Kuralımız işlem yapabilmem için ilk karakter her zaman G
+	if string(line[0]) == ";" {
+		return "", nil
+	}
+
 	// Satırın son kısımda M ile başlayan satır varsa onu sil
 	lastline := split_line_with_space[len(split_line_with_space)-1]
+
+	// X'ler belirtildiği kadar artırılacak
+	for _, v := range split_line_with_space {
+
+		first_char := v[0]
+
+		if string(first_char) != "X" {
+			continue
+		}
+
+		// TODO: Bu alanda kullanıcıdan aldığım x değerini her x satırına ekleyeceğim
+
+	}
 
 	// Satırın sonında E veya e harfi gördüğünde silecek
 	if string(lastline[0]) == "E" || string(lastline[0]) == "e" {
@@ -96,10 +130,10 @@ func worker(line string) (string, error) {
 		split_line_with_space = append(split_line_with_space, "M8")
 	}
 
-	// Parçaladığım veriyi yeniden birleştiriyoruz
+	// Paçalanan veri yeniden birleştiriyoruz
 	join_current_line := strings.Join(split_line_with_space, " ")
 
-	// Güncellenen satırı yeniden gönderiyorum
+	// İşlem yaptığımız satırı kaydetmek için gönderiyorum
 	return join_current_line, nil
 
 }
